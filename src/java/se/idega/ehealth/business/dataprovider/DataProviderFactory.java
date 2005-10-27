ca7a26cf258ef39f4a41df0ea7fd525ad5826cdd
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2005-10-27 14:14:08 +0000

    Fixing compile error

--- src/java/se/idega/ehealth/business/dataprovider/DataProviderFactory.java
+++ src/java/se/idega/ehealth/business/dataprovider/DataProviderFactory.java
@@ -6,16 +6,21 @@ package se.idega.ehealth.business.dataprovider;
  * <p>
  * TODO Maris_O Describe Type DataProviderFactory
  * </p>
- * Last modified: $Date: 2005/10/27 12:39:55 $ by $Author: mariso $
+ * Last modified: $Date: 2005/10/27 14:14:08 $ by $Author: tryggvil $
  * 
  * @author <a href="mailto:Maris.Orbidans@idega.lv">Maris.Orbidans</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class DataProviderFactory
 {
 
     public DataProvider createDataProvider()
     {
-        return new se.idega.ehealth.npo.dataprovider.NPODataProvider();
+    		try {
+				return (DataProvider)Class.forName("se.idega.ehealth.npo.dataprovider.NPODataProvider").newInstance();
+			}
+			catch (Exception e) {
+				throw new RuntimeException(e);
+			}
     }
 }
