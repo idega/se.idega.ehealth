c5e555762e4c75af815df5b356d5526e00f324e6
Maris Orbidans <Maris.Orbidans@idega.lv>
2006-03-06 16:43:39 +0000

    *** empty log message ***

--- src/java/se/idega/ehealth/business/dataprovider/DataProvider.java
+++ src/java/se/idega/ehealth/business/dataprovider/DataProvider.java
@@ -1,27 +1,28 @@
 package se.idega.ehealth.business.dataprovider;
 
-import java.rmi.RemoteException;
-import java.util.ArrayList;
-import java.util.List;
-import se.carelink.webservices.npo.ArrayOfKuvert;
-import se.carelink.webservices.npo.HamtaVarddokumentIndexReply;
-import se.carelink.webservices.npo.Kuvert;
 import se.idega.ehealth.business.dataprovider.valueobj.PersonalInfo;
 import se.idega.ehealth.presentation.valueobjects.ContactsValueObject;
+import se.idega.ehealth.presentation.valueobjects.DiagnosisValueObject;
+import se.idega.ehealth.presentation.valueobjects.MedicinesValueObject;
+import se.idega.ehealth.presentation.valueobjects.ReferralsOfXrayValueObject;
+import se.idega.ehealth.presentation.valueobjects.ReferralsValueObject;
 
 /**
  * 
  * <p>
  * TODO Maris_O Describe Type DataProvider
  * </p>
- * Last modified: $Date: 2005/12/07 16:29:38 $ by $Author: mariso $
+ * Last modified: $Date: 2006/03/06 16:43:39 $ by $Author: mariso $
  * 
  * @author <a href="mailto:Maris.Orbidans@idega.lv">Maris.Orbidans</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public interface DataProvider
 {
-    public Object readReferalls(String personId, String contactId);
     public ContactsValueObject[] readContacts(String personId);
+    public ReferralsValueObject[] readReferralsOfClinical(String personId);    
+    public ReferralsOfXrayValueObject[] readReferralsOfXray(String personId);
+    public MedicinesValueObject[] readMedicines(String personId);
+    public DiagnosisValueObject[] readDiagnosis(String personId) ;   
     public PersonalInfo readPersonalInfo(String personId);
 }
